e101f5f50c871510249d788c1e3584e06c0ad2aa
FernandaAguirre17 <utm23090769@utma.edu.mx>
2024-07-17 12:20:43 -0600

    Initial commit.

---
+++ src/JessNew_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { JessNew_backend } from 'declarations/JessNew_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    JessNew_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
